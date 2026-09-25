32fd904ac68a52144f0b523c4679c8b47a349863
ThaiPy0605 <xuanthainguyen652005@gmail.com>
2026-09-25 15:16:50 +0700

    feat: add contact request error codes

--- src/core/api/error-code.ts
+++ src/core/api/error-code.ts
@@ -5,6 +5,8 @@ export const ErrorCodes = {
   requestTimeout: 'REQUEST_TIMEOUT',
   sessionExpired: 'SESSION_EXPIRED',
   slotUnavailable: 'SLOT_UNAVAILABLE',
+  contactRequestLimitReached: 'CONTACT_REQUEST_LIMIT_REACHED',
+  invalidContactRequestState: 'INVALID_CONTACT_REQUEST_STATE',
 } as const
 
 export type KnownErrorCode = (typeof ErrorCodes)[keyof typeof ErrorCodes]
